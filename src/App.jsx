ee9f78a36ebff129207e38ca52e12f00e7334b0f
Josh Shavers <shaversj@gmail.com>
2023-09-21 07:55:12 -0500

    project setup + html

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+
+function App() {
+
+
+  return (
+    <>
+        <main>
+            <div>
+                <h1>10,000+ of our users love our products.</h1>
+                <p>We only provide great products combined with excellent customer service.
+                    See what our satisfied customers are saying about our services.</p>
+            </div>
+
+            <div>
+                <h2>Rated 5 Stars in Reviews</h2>
+                <h2>Rated 5 Stars in Report Guru</h2>
+                <h2>Rated 5 Stars in BestTech</h2>
+            </div>
+
+            <div>
+                <div>
+                    <h2>Colton Smith</h2>
+                    <h3>Verified Buyer</h3>
+                    <p>
+                        "We needed the same printed design as the one we had ordered a week prior.
+                        Not only did they find the original order, but we also received it in time.
+                        Excellent!"
+                    </p>
+
+                </div>
+
+                <div>
+                    <h2>Irene Roberts</h2>
+                    <h3>Verified Buyer</h3>
+                    <p>
+                        "Customer service is always excellent and very quick turn around. Completely
+                        delighted with the simplicity of the purchase and the speed of delivery."
+                    </p>
+
+                </div>
+
+                <div>
+                    <h2>Anne Wallace</h2>
+                    <h3>Verified Buyer</h3>
+                    <p>
+                        "Put an order with this company and can only praise them for the very high
+                        standard. Will definitely use them again and recommend them to everyone!"
+                    </p>
+                </div>
+            </div>
+        </main>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': "375px",
+        'lg': '1440px',
+      },
+      colors: {
+        'very-dark-magenta': 'hsl(300, 43%, 22%)',
+        'soft-pink': 'hsl(333, 80%, 67%)',
+        'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
+        'Light Grayish Magenta': 'hsl(300, 24%, 96%)',
+        'custom-white': 'hsl(0, 0%, 100%)'
+      },
+      fontFamily: {
+        'League-Spartan': ['League Spartan', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
