51f661f474a63869a5b38a9cd826daae9958b070
Josh Shavers <shaversj@gmail.com>
2023-09-24 09:59:36 -0500

    Mobile layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,114 @@
 
+import coltonProfile from './assets/images/image-colton.jpg'
+import anneProfile from './assets/images/image-anne.jpg'
+import ireneProfile from './assets/images/image-irene.jpg'
 function App() {
 
 
   return (
     <>
-        <main>
+        <main className={"max-w-screen-sm bg-white mx-auto min-h-screen px-5 font-League-Spartan space-y-8 first:pt-[4.6rem]"}>
             <div>
-                <h1>10,000+ of our users love our products.</h1>
-                <p>We only provide great products combined with excellent customer service.
+                <h1 className={"text-very-dark-magenta text-[33px] font-bold leading-[1.6rem] text-center"}>10,000+ of our users love our products.</h1>
+                <p className={"pt-6 text-dark-grayish-magenta text-center leading-[1.38rem] text-[15px] font-medium"}>We only provide great products combined with excellent customer service.
                     See what our satisfied customers are saying about our services.</p>
             </div>
 
-            <div>
-                <h2>Rated 5 Stars in Reviews</h2>
-                <h2>Rated 5 Stars in Report Guru</h2>
-                <h2>Rated 5 Stars in BestTech</h2>
+
+
+            <div className={"w-full space-y-4"}>
+                <div className={"bg-light-grayish-magenta space-y-2 py-3 rounded-lg flex flex-col items-center"}>
+                    <div className={"flex gap-x-2 justify-center"}>
+                        <svg className={""} xmlns="http://www.w3.org/2000/svg"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                    </div>
+                    <h2 className={"text-very-dark-magenta font-bold text-[15px] text-center"}>Rated 5 Stars in Reviews</h2>
+                </div>
+                <div className={"bg-light-grayish-magenta space-y-2 py-2 rounded-lg"}>
+                    <div className={"flex gap-x-2 justify-center"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                    </div>
+                    <h2 className={"text-very-dark-magenta font-bold text-[15px] text-center"}>Rated 5 Stars in Report Guru</h2>
+                </div>
+                <div className={"bg-light-grayish-magenta space-y-2 py-2 rounded-lg"}>
+                    <div className={"flex gap-x-2 justify-center"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#EF9546" d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"/></svg>
+                    </div>
+                    <h2 className={"text-very-dark-magenta font-bold text-[15px] text-center"}>Rated 5 Stars in BestTech</h2>
+                </div>
             </div>
 
-            <div>
-                <div>
-                    <h2>Colton Smith</h2>
-                    <h3>Verified Buyer</h3>
-                    <p>
-                        "We needed the same printed design as the one we had ordered a week prior.
+            <div className={"space-y-4"}>
+                <div className={"bg-very-dark-magenta p-7 space-y-5 rounded-lg"}>
+                    <div className={"flex items-center space-x-5"}>
+                        <div className={""}>
+                            <img className={"rounded-full w-[36px] h-[36px]"} src={coltonProfile} alt={""} />
+                        </div>
+                        <div className={""}>
+                            <span className={"text-custom-white text-sm font-semibold tracking-wider"}>Colton Smith</span>
+                            <span className={"text-soft-pink text-sm leading-[.7rem] block tracking-wider"}>Verified Buyer</span>
+                        </div>
+
+                    </div>
+
+                    <p className={"text-custom-white text-[14.5px] font-thin leading-5"}>
+                        " We needed the same printed design as the one we had ordered a week prior.
                         Not only did they find the original order, but we also received it in time.
-                        Excellent!"
+                        Excellent! "
                     </p>
 
                 </div>
 
-                <div>
-                    <h2>Irene Roberts</h2>
-                    <h3>Verified Buyer</h3>
-                    <p>
-                        "Customer service is always excellent and very quick turn around. Completely
-                        delighted with the simplicity of the purchase and the speed of delivery."
+                <div className={"bg-very-dark-magenta p-7 space-y-5 rounded-lg"}>
+                    <div className={"flex items-center space-x-5"}>
+                        <div className={""}>
+                            <img className={"rounded-full w-[36px] h-[36px]"} src={ireneProfile} alt={""} />
+                        </div>
+                        <div className={""}>
+                            <span className={"text-custom-white text-sm font-semibold tracking-wider"}>Irene Roberts</span>
+                            <span className={"text-soft-pink text-sm leading-[.7rem] block tracking-wider"}>Verified Buyer</span>
+                        </div>
+
+                    </div>
+
+                    <p className={"text-custom-white text-[14.5px] font-thin leading-5"}>
+                        " Customer service is always excellent and very quick turn around. Completely
+                        delighted with the simplicity of the purchase and the speed of delivery. "
                     </p>
 
                 </div>
 
-                <div>
-                    <h2>Anne Wallace</h2>
-                    <h3>Verified Buyer</h3>
-                    <p>
-                        "Put an order with this company and can only praise them for the very high
-                        standard. Will definitely use them again and recommend them to everyone!"
+                <div className={"bg-very-dark-magenta p-7 space-y-5 rounded-lg"}>
+                    <div className={"flex items-center space-x-5"}>
+                        <div className={""}>
+                            <img className={"rounded-full w-[36px] h-[36px]"} src={anneProfile} alt={""} />
+                        </div>
+                        <div className={""}>
+                            <span className={"text-custom-white text-sm font-semibold tracking-wider"}>Anne Wallace</span>
+                            <span className={"text-soft-pink text-sm leading-[.7rem] block tracking-wider"}>Verified Buyer</span>
+                        </div>
+
+                    </div>
+
+                    <p className={"text-custom-white text-[14.5px] font-thin leading-5"}>
+                        " Put an order with this company and can only praise them for the very high
+                        standard. Will definitely use them again and recommend them to everyone! "
                     </p>
+
                 </div>
+
+
             </div>
         </main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ export default {
   theme: {
     extend: {
       screens: {
-        'sm': "375px",
+        'sm': "325px",
         'lg': '1440px',
       },
       colors: {
         'very-dark-magenta': 'hsl(300, 43%, 22%)',
         'soft-pink': 'hsl(333, 80%, 67%)',
         'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
-        'Light Grayish Magenta': 'hsl(300, 24%, 96%)',
+        'light-grayish-magenta': 'hsl(300, 24%, 96%)',
         'custom-white': 'hsl(0, 0%, 100%)'
       },
       fontFamily: {
