27ebe0735564933b1ee3b396b292f93fef08b12e
Josh Shavers <shaversj@gmail.com>
2023-10-06 08:30:30 -0500

    Revert dynamic class changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,8 @@ const Card = ({ id, name, buyerType, review, profile }) => {
     2: "bg-very-dark-magenta px-[1.7rem] py-8 space-y-5 rounded-lg lg:w-[21.875rem] h-[14.563rem] lg:px-[2rem] lg:py-9 lg:space-y-7 lg:relative lg:top-[2rem]"
   }
 
-  let parentCardClass = `bg-very-dark-magenta px-[1.7rem] py-8 space-y-5 rounded-lg lg:w-[21.875rem] h-[14.563rem] lg:px-[2rem] lg:py-9 lg:space-y-7 lg:relative lg:top-[${id}rem]`
-
   return (
-    <div className={parentCardClass}>
+    <div className={cardPicker[id]}>
       <div className={"flex items-center space-x-5 lg:space-x-6"}>
         <div className={""}>
           <img className={"rounded-full w-[2.25rem] h-[2.25rem] lg:w-[2.5rem] lg:h-[2.5rem]"} src={profile} alt={""} />
