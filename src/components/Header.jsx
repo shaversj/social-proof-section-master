adc469cfb04dd49be79b041014ae10d1603d1817
Josh Shavers <shaversj@gmail.com>
2023-10-06 08:00:40 -0500

    Revert "Convert px to rem"
    This reverts commit 4085785d800be4cf33faa26bce568231424f7a49.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
     <div className={"lg:flex lg:flex-col lg:justify-center lg:pr-12"}>
-      <h1 className={"text-very-dark-magenta text-[2.063rem] font-bold leading-[1.6rem] text-center lg:text-[3.438rem] lg:font-bold lg:leading-[3rem] lg:text-left lg:tracking-tight"}>
+      <h1 className={"text-very-dark-magenta text-[33px] font-bold leading-[1.6rem] text-center lg:text-[55px] lg:font-bold lg:leading-[3rem] lg:text-left lg:tracking-tight"}>
         10,000+ of our users love our products.
       </h1>
-      <p className={"pt-6 text-dark-grayish-magenta text-center leading-[1.38rem] text-[0.938rem] font-medium lg:text-left lg:text-[1.125rem] lg:leading-6 lg:pt-7 lg:font-normal"}>
+      <p className={"pt-6 text-dark-grayish-magenta text-center leading-[1.38rem] text-[15px] font-medium lg:text-left lg:text-[18px] lg:leading-6 lg:pt-7 lg:font-normal"}>
         We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.
       </p>
     </div>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,7 +3,7 @@ const Rating = ({ id, stars, rating }) => {
     let stars = []
     for (let i = 0; i < numOfStars; i++) {
       stars.push(
-        <svg className={"w-[1rem] h-[0.938rem] lg:w-[1.063rem] lg:h-[1rem]"} xmlns="http://www.w3.org/2000/svg">
+        <svg className={"w-[16px] h-[15px] lg:w-[17px] lg:h-[16px]"} xmlns="http://www.w3.org/2000/svg">
           <path
             fill="#EF9546"
             d="M16.539 6.097a.297.297 0 0 0-.24-.202l-5.36-.779L8.542.26a.296.296 0 0 0-.53 0L5.613 5.117l-5.36.779a.297.297 0 0 0-.165.505l3.88 3.78-.917 5.34a.297.297 0 0 0 .43.312l4.795-2.52 4.794 2.52a.296.296 0 0 0 .43-.313l-.916-5.338 3.88-3.782c.08-.08.11-.197.075-.304z"
@@ -15,15 +15,17 @@ const Rating = ({ id, stars, rating }) => {
     return <div className={"flex gap-x-2 justify-center"}>{stars}</div>
   }
 
-  let parentRatingClass = `bg-light-grayish-magenta space-y-2 py-3 rounded-lg flex flex-col items-center lg:w-[27.688rem] lg:h-[3.375rem] lg:flex lg:flex-row lg:space-x-8 lg:pl-8 lg:space-y-0 lg:ml-[${
-    id * 3
-  }rem]`
+  let ratingPicker = {
+    0: "bg-light-grayish-magenta space-y-2 py-3 rounded-lg flex flex-col items-center lg:w-[443px] lg:h-[54px] lg:flex lg:flex-row lg:space-x-8 lg:pl-8 lg:space-y-0 lg:ml-[0px]",
+    1: "bg-light-grayish-magenta space-y-2 py-3 rounded-lg flex flex-col items-center lg:w-[443px] lg:h-[54px] lg:flex lg:flex-row lg:space-x-8 lg:pl-8 lg:space-y-0 lg:ml-[48px]",
+    2: "bg-light-grayish-magenta space-y-2 py-3 rounded-lg flex flex-col items-center lg:w-[443px] lg:h-[54px] lg:flex lg:flex-row lg:space-x-8 lg:pl-8 lg:space-y-0 lg:ml-[96px]"
+  }
 
   return (
-    <div className={parentRatingClass}>
+    <div className={ratingPicker[id]}>
       {ratingStars(stars)}
       <div className={""}>
-        <h2 className={"text-very-dark-magenta font-bold text-[0.938rem] text-center lg:text-[1.063rem] lg:leading-3"}>{rating}</h2>
+        <h2 className={"text-very-dark-magenta font-bold text-[15px] text-center lg:text-[17px] lg:leading-3"}>{rating}</h2>
       </div>
     </div>
   )
