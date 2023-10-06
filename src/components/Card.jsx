1a8950cca60999bbb336b01c3567b17d2f8c3947
Josh Shavers <shaversj@gmail.com>
2023-10-06 08:00:53 -0500

    Revert "Convert card px to rem"
    This reverts commit bd3be816f244ca57d8b328c87373bc37f2ea0409.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,22 @@
 const Card = ({ id, name, buyerType, review, profile }) => {
   let cardPicker = {
-    0: "bg-very-dark-magenta px-[1.7rem] py-8 space-y-5 rounded-lg lg:w-[21.875rem] h-[14.563rem] lg:px-[2rem] lg:py-9 lg:space-y-7 lg:relative lg:top-[0rem]",
-    1: "bg-very-dark-magenta px-[1.7rem] py-8 space-y-5 rounded-lg lg:w-[21.875rem] h-[14.563rem] lg:px-[2rem] lg:py-9 lg:space-y-7 lg:relative lg:top-[1rem]",
-    2: "bg-very-dark-magenta px-[1.7rem] py-8 space-y-5 rounded-lg lg:w-[21.875rem] h-[14.563rem] lg:px-[2rem] lg:py-9 lg:space-y-7 lg:relative lg:top-[2rem]"
+    0: "bg-very-dark-magenta px-[1.7rem] py-8 space-y-5 rounded-lg lg:w-[350px] h-[233px] lg:px-[2rem] lg:py-9 lg:space-y-7 lg:relative lg:top-[0px]",
+    1: "bg-very-dark-magenta px-[1.7rem] py-8 space-y-5 rounded-lg lg:w-[350px] h-[233px] lg:px-[2rem] lg:py-9 lg:space-y-7 lg:relative lg:top-[16px]",
+    2: "bg-very-dark-magenta px-[1.7rem] py-8 space-y-5 rounded-lg lg:w-[350px] h-[233px] lg:px-[2rem] lg:py-9 lg:space-y-7 lg:relative lg:top-[32px]"
   }
 
-  let parentCardClass = `bg-very-dark-magenta px-[1.7rem] py-8 space-y-5 rounded-lg lg:w-[21.875rem] h-[14.563rem] lg:px-[2rem] lg:py-9 lg:space-y-7 lg:relative lg:top-[${id}rem]`
-
   return (
-    <div className={parentCardClass}>
+    <div className={cardPicker[id]}>
       <div className={"flex items-center space-x-5 lg:space-x-6"}>
         <div className={""}>
-          <img className={"rounded-full w-[2.25rem] h-[2.25rem] lg:w-[2.5rem] lg:h-[2.5rem]"} src={profile} alt={""} />
+          <img className={"rounded-full w-[36px] h-[36px] lg:w-[40px] lg:h-[40px]"} src={profile} alt={""} />
         </div>
         <div className={""}>
-          <span className={"text-white text-sm font-semibold tracking-wider lg:text-[0.969rem] lg:leading-none"}>{name}</span>
-          <span className={"text-soft-pink text-sm leading-[.7rem] block tracking-wider lg:text-[0.969rem] lg:leading-none"}>{buyerType}</span>
+          <span className={"text-white text-sm font-semibold tracking-wider lg:text-[15.5px] lg:leading-none"}>{name}</span>
+          <span className={"text-soft-pink text-sm leading-[.7rem] block tracking-wider lg:text-[15.5px] lg:leading-none"}>{buyerType}</span>
         </div>
       </div>
-      <p className={"text-white text-[0.925rem] font-thin leading-5 lg:text-[1.069rem] lg:leading-[1.4rem]"}>" {review} "</p>
+      <p className={"text-white text-[14.8px] font-thin leading-5 lg:text-[17.1px] lg:leading-[1.4rem]"}>" {review} "</p>
     </div>
   )
 }
